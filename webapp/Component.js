715b5c95b8c3724e3b0fee232ba0024c3cb0ded6
Singh Jaspreet <jaspreet.singh02@sap.com>
2018-10-16 18:49:19 +0000

    Commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("ProductionVolumes.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("ProductionVolumes.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
